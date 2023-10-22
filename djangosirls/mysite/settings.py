143c610c5d6cbc5491ee0f8be713de72c287a1ce
Serwaa <c.akotonimo@alustudent.com>
2023-10-22 22:09:51 +0200

    i guess it worked

--- djangosirls/mysite/settings.py
+++ djangosirls/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-kl_q5l6w79@(7(a$1k)a=hp)gaxdk)f2vrcf@hz(o6iem%5@jl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Kigali'
 
 USE_I18N = True
 
@@ -118,8 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
